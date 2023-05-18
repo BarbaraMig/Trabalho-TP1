7878e924782449dedd18d0eda12d5d7d9982cf1a
HaruRyuzaki <131807007+HaruRyuzaki@users.noreply.github.com>
2023-05-18 11:03:04 -0300

    Update arctan.c

--- arctan.c
+++ arctan.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-int main(){
-//
+
 //Crie um programa em C que lê primeiramente um valor x (|x| < 1), e em seguida um
 //valor n (n ≥ 0) onde n é o número de termos da série e imprime o valor do arco-tangente,
 //calculado de acordo com a fórmula acima.
@@ -12,16 +11,41 @@ int main(){
 //C/C++ nem em nenhuma outra biblioteca.
 //d) Não poderão ser usados vetores nem qualquer outro tipo de estrutura de dados, somente variáveis simples.
 //e) Todo o código deverá estar implementado na função main, sem o uso de funções auxiliares, com a exceção das funções scanf, printf e fflush.
-    float x, n;
+int main(){
+    double x;
+    int N = -1;
+    int pot = 1.0;
+    int cont = 0;
     printf("insira o valor de x: ");
-    scanf("%f", &x);
-    printf("insira o valor de n: ");
-    scanf("%f", &n);
-    float arctan;
+    scanf("%f",&x);
+    while( x>=1 || x<=-1 ){
+        printf("insira novamente o valor de x: ");
+        scanf("%f", &x);
+    }//primeira parte parece certa
+
+        printf("insira o valor de n: ");
+        scanf("%d",&N);
+    while( N<0 ){
+        printf("insira novamente o valor de N: ");
+        scanf("%d", &N);
+    }
 
 
+int ip;
 
+while(i<N){
 
+//o loop da potência
+for(cont; cont<N; cont++){
+int k = (2 * cont) + 1;
+    for(ip = 0; ip<k; ip++){
+        pot = x * x;
+    }
+    x = x + (x * pot);
+}
+}
+
+printf("%lf", x);
 
     return 0;
 }
